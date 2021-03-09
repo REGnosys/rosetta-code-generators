7d400f98cc48cb652345a768fa47efc4c283ca51
jim-h-wang <jim.wang@regnosys.com>
2021-03-09 08:05:47 +0000

    Revert "Merge pull request #72 from RandalP/master"
    This reverts commit 05b1848dcee94883b1a56934c73d05d4597ad455, reversing
    changes made to 83c09ef0e204ff01a0bdc47749b6c2e2118a6195.

--- c-sharp/Rosetta/OneOf.cs
+++ c-sharp/Rosetta/OneOf.cs
@@ -6,7 +6,7 @@
     /// Specifies that only one of the properties of the class should ever be set.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public class OneOf : Attribute
+    public class OneOfAttribute : Attribute
     {
 
     }
