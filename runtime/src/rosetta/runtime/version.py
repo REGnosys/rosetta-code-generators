7249e556908e8e61e1e2f430c90251b382dd0a90
dan <daniel.schwartz.nyc@gmail.com>
2023-06-14 18:01:43 -0400

    clean up - removed extra runtime directory
    This reverts commit 757d3a27ae3713c6d1a96f7d3432d218ab0fc719.

--- runtime/src/rosetta/runtime/version.py
+++
@@ -1,9 +0,0 @@
-'''PEP 396 module version attribute in PEP 440 version format'''
-
-version = (1, 0, 0, 0)
-
-version_str = f"{'.'.join(str(i) for i in version[:3])}-{version[3]}"  # pylint: disable=invalid-name
-
-__version__ = '.'.join(str(i) for i in version[:3])
-
-# EOF
