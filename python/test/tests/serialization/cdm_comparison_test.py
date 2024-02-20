56ff2eba870a1ee1d4d13e76fe0a9cace94c5fb6
Plamen Neykov <plamen.neykov@cloudrisk.eu>
2024-02-20 14:28:32 +0000

    fixed the dict comparison function and used model_dum_json as pydantic 2.x.x compliant

--- python/test/tests/serialization/cdm_comparison_test.py
+++ python/test/tests/serialization/cdm_comparison_test.py
@@ -20,7 +20,7 @@ def cdm_comparison_test_from_file(path, class_name):
 def cdm_comparison_test_from_string(json_str, class_name):
     '''loads the json from a string and runs the comparison'''
     cdm_object = class_name.parse_raw(json_str)
-    json_data_out = cdm_object.json(exclude_defaults=True, indent=4)
+    json_data_out = cdm_object.model_dump_json(exclude_defaults=True, indent=4)
     orig_dict = json.loads(json_str)
     generated_dict = json.loads(json_data_out)    
     assert dict_comp(orig_dict, generated_dict), "Failed corrected dict comparison"
