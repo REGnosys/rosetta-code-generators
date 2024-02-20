a575b6b3819329509fe1edaa372315c4f9c8b296
dschwartznyc <daniel.schwartz@ftadvisory.co>
2024-02-20 14:39:52 +0000

    updated test and python build including adding corrected EUR-Vanilla...json

---
+++ python/test/tests/serialization/check_for_valid_cdm.py
@@ -0,0 +1,47 @@
+'''find all valid and invalid CDM files (.json) by recursing a directory'''
+import sys
+import os
+import glob
+import json
+from pathlib import Path
+from pydantic import ValidationError
+from cdm.event.common.TradeState import TradeState
+from cdm.version import __build_time__, __version__
+from dict_comp import dict_comp
+
+dirPath = os.path.dirname(__file__)
+sys.path.append(os.path.join(dirPath))
+
+def cdm_comparison_test_from_file(file_name, class_name):
+    '''loads the json from a file and runs the comparison'''
+    json_str  = Path(file_name).read_text()
+    json_dict = json.loads (json_str)
+    try:
+        cdm_object = class_name.parse_raw(json_str)
+        json_data_out = cdm_object.json(exclude_defaults=True, indent=4)
+        generated_dict = json.loads(json_data_out)
+        if dict_comp(json_dict, generated_dict):
+            return True
+    except ValidationError:
+        pass
+    return False
+
+if __name__ == "__main__":
+    valid_files = []
+    invalid_files = []
+    file_loc = '.' if (len(sys.argv)<2) else sys.argv[1]
+    print('checking files using cdm version: ', __version__, ' built at:',__build_time__)
+    for dir_name, subdir_list, file_list in os.walk(file_loc):
+        cdm_files = glob.glob(dir_name + os.sep + '*.json')
+        print('in dir:', dir_name, ' found # files:', len(cdm_files))
+        for cdm in cdm_files:
+            if cdm_comparison_test_from_file(cdm,TradeState):
+                valid_files.append (cdm)
+            else:
+                invalid_files.append(cdm)
+    print('valid files')
+    for cdm in valid_files:
+        print (cdm)
+    print('invalid files')
+    for cdm in invalid_files:
+        print (cdm)
