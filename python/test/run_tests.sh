12b3dab133f118c06fe2e1a9f02994be3e6969e5
dschwartznyc <daniel.schwartz@ftadvisory.co>
2024-04-03 18:26:38 -0400

    More pre-release cleanup including release notes

--- python/test/run_tests.sh
+++ python/test/run_tests.sh
@@ -9,15 +9,16 @@ fi
 ACDIR=$($PYEXE -c "import sys;print('Scripts' if sys.platform.startswith('win') else 'bin')")
 export PYTHONDONTWRITEBYTECODE=1
 
-$PYEXE -m venv --clear .pydevenv
-source .pydevenv/$ACDIR/activate
-
 MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROSETTARUNTIMEDIR="../src/main/resources/runtime"
 PYTHONCDMDIR="../target/python"
 cd $MYPATH
+$PYEXE -m venv --clear .pydevenv
+source .pydevenv/$ACDIR/activate
 $PYEXE -m pip install pydantic
 $PYEXE -m pip install pytest
 $PYEXE -m pip install $MYPATH/$ROSETTARUNTIMEDIR/rosetta_runtime-2.0.0-py3-none-any.whl 
 $PYEXE -m pip install $MYPATH/$PYTHONCDMDIR/python_cdm-0.0.0-py3-none-any.whl
 pytest -p no:cacheprovider
+
+rm -rf .pydevenv
