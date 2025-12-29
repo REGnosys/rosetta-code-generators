2c3cfdc8dfc28d9dda66447a090b382a2613c743
Hugo Hills <hugo.hills@regnosys.com>
2025-12-29 17:13:17 +0000

    Remove python implementation as it has been moved to a separate repo

--- python/src/main/resources/runtime/src/rosetta/runtime/__init__.py
+++
@@ -1,5 +0,0 @@
-'''rosetta runtime package'''
-# PEP 396 module version attribute in PEP 440 version format
-from .version import __version__
-
-# EOF
--- python/src/main/resources/runtime/src/rosetta/runtime/version.py
+++
@@ -1,9 +0,0 @@
-'''PEP 396 module version attribute in PEP 440 version format'''
-version = (2, 1, 0, 0)
-
-# pylint: disable=invalid-name
-version_str = f"{'.'.join(str(i) for i in version[:3])}-{version[3]}"
-
-__version__ = '.'.join(str(i) for i in version[:3])
-
-# EOF
